169379ac00a7a9f3bd89e3098be954081d19596f
Marchi8 <luandeoliveirasoares@gmail.com>
2024-07-18 15:38:13 -0300

    Updated responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -45,52 +45,47 @@ function App() {
 
   return (
     <div className="App">
-      {/* <Routes basename="/01">
-        <Route path="/:gtin" element={ <Product/> }/>
-      </Routes> */}
-      <Main>
-      {apiProduct.photo?  
-        <div style={{marginTop:70,display:'flex',alignItems:'center',justifyContent:'flex-start',flexDirection:'column',maxWidth:342,width:'90vw',paddingBottom: 55}}>
-        <img style={{position:'relative',top:-20}} src={HomeLogo} alt="NuRótulo Logo" />
-        <div style={{display:'flex',flexDirection:'row', alignItems:'center',marginBottom:10,}}>
-          <img className='productImg' src={apiProduct?.photo?.url} alt="product image" />
-          <div style={{textAlign:'left',marginLeft:10,display:'flex',justifyContent:'start',flexDirection:"column"}}>
-            <p style={{fontSize:16,margin:0,fontWeight:'bold'}}>RÓTULO CERTIFICADO</p>
-            <p style={{fontSize:13,margin:0,fontWeight:'500'}}>DATA DE EMISSÃO: {apiProduct.valid}.</p>
-            <p style={{fontSize:13,margin:0,fontWeight:'500'}}>{apiProduct?.name}</p>
-          </div>
-          <img src={CertifiedLogo} alt="Certified Logo" />
-        </div>
-
-        <div style={{maxWidth:352, width:'90vw', textAlign:'left',marginBottom:15}}>
-            <h4 style={{fontSize:13,maxWidth:330,fontWeight:'500'}}>DATA DE EMISSÃO: {apiProduct.valid}.</h4>
-            <h4 style={{fontSize:13,maxWidth:330,fontWeight:'500'}}>Este RÓTULO passou por curadoria referente às boas práticas de rotulagem, conforme legislação vigente, e foi APROVADO.</h4>
-            <h4 style={{fontSize:13,maxWidth:330,fontWeight:'500'}}>O Selo ESG de Integridade do Rótulo é um certificado de responsabilidade social.</h4>
-            {/* <h4 style={{fontSize:13,maxWidth:330,fontWeight:'500'}}>Encontre esse produto e produtos similares, direto na LOJA VIRTUAL e receba na sua casa.</h4> */}
-        </div>
-
-        <div>
-          <div style={{display:'flex',gap:24,marginBottom:30}}>
-            {relatedProducts.length?
-            relatedProducts.map((product,index)=>(
-              <button onClick={() => handleClick(product.ean)} key={index} style={{width:65,height:86,borderRadius:15,backgroundColor:'#FFFFFF', display:'flex', alignItems:'center',justifyContent:'center'}}>
-                <img style={{width:65,height:86,borderRadius:15,backgroundColor:'#FFFFFF'}} src={product?.photo?.url} alt="product suggestion image" />
-              </button>
-            )) : null}
+    <Main>
+      {apiProduct.photo ?  
+        <div className="container">
+          <img className="logo" src={HomeLogo} alt="NuRótulo Logo" />
+          <div className="product-info">
+            <img className="productImg" src={apiProduct?.photo?.url} alt="product image" />
+            <div className="product-details">
+              <p className="title">RÓTULO CERTIFICADO</p>
+              <p className="details">DATA DE EMISSÃO: {apiProduct.valid}.</p>
+              <p className="details">{apiProduct?.name}</p>
+            </div>
+            <img className="certifiedLogo" src={CertifiedLogo} alt="Certified Logo" />
           </div>
 
-          <div style={{display:'flex',alignItems:'center',justifyContent:'center'}}>
-            <img src={ClickHereLogo} alt="Click here logo" />
-            <a href={apiProduct.site} target="_blank" rel="noopener noreferrer" style={{backgroundColor:'#FFFFFF', width:85,height:85,borderRadius:100, border:'2px solid white',display:'flex',alignItems:'center',justifyContent:'center'}}>
-              <img style={{width:80,height:80, borderRadius:100,}}  src={apiProduct?.siteLogo?.url} alt="Company logo" />
-            </a>
+          <div className="description">
+            <h4 className="details">DATA DE EMISSÃO: {apiProduct.valid}.</h4>
+            <h4 className="details">Este RÓTULO passou por curadoria referente às boas práticas de rotulagem, conforme legislação vigente, e foi APROVADO.</h4>
+            <h4 className="details">O Selo ESG de Integridade do Rótulo é um certificado de responsabilidade social.</h4>
+          </div>
+
+          <div>
+            <div className="related-products">
+              {relatedProducts.length ?
+              relatedProducts.map((product, index) => (
+                <button onClick={() => handleClick(product.ean)} key={index} className="related-product-button">
+                  <img className="related-product-img" src={product?.photo?.url} alt="product suggestion image" />
+                </button>
+              )) : null}
+            </div>
+
+            <div className="click-here">
+              <img src={ClickHereLogo} alt="Click here logo" />
+              <a href={apiProduct.site} target="_blank" rel="noopener noreferrer" className="click-here-button">
+                <img className="site-logo" src={apiProduct?.siteLogo?.url} alt="Company logo" />
+              </a>
+            </div>
           </div>
         </div>
-      </div>
-      :  <LoadingSpinner/>}
-    
+      :  <LoadingSpinner />}
     </Main>
-    </div>
+  </div>
   );
 }
 
